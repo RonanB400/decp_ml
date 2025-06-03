6c0cce82ae379d66fb6c71e29ccf63f280ec31c4
Paul Colas <paulcolas00@gmail.com>
2025-06-03 17:08:40 +0200

    ajout de la fonction de selection des colonnes

--- scripts/preprocess_montant.py
+++ scripts/preprocess_montant.py
@@ -3,6 +3,16 @@ import pandas as pd
 from preprocess_cpv import extract_cpv_hierarchy_level, add_cpv_hierarchy_column
 
 
+#selection des colonnes
+def columns_selection(df):
+    df = df[['procedure', 'dureeMois','nature', 'formePrix', 'offresRecues', 'ccag',
+          'sousTraitanceDeclaree', 'typeGroupementOperateurs', 'tauxAvance', 'origineUE',
+          'origineFrance', 'idAccordCadre']]
+    return df
+
+#df = df
+
+
 #retrait des marchés supérieurs à 50 millions et inférieur à 20 milles.
 def drop_outliers(df):
     df.drop(df[df['montant'] > 50000000].index, inplace=True)
@@ -12,7 +22,6 @@ def drop_outliers(df):
 
 #ajout d'une colonne avec les 2 premiers chiffres du CPV, et les trois premiers si c'est 45 ou 71
 def cpv_2et3 (df):
-
     df = add_cpv_hierarchy_column(df)
     df3 = add_cpv_hierarchy_column(df, level=3)
     df["codeCPV_3"] = df3["codeCPV_3"]
@@ -20,5 +29,16 @@ def cpv_2et3 (df):
         df["codeCPV_2"] = df["codeCPV_3"]
     if df["codeCPV_2"] == "71000000":
         df["codeCPV_2"] = df["codeCPV_3"]
-        
+
     return df
+
+
+#date --> to datetime --> juste année ?
+
+
+
+#scaling dureeMois & offresRecues
+
+
+
+#missing data
