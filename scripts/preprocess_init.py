7d51198ba6e56721fc969eec6670beb6fa6d6699
Paul Colas <paulcolas00@gmail.com>
2025-06-04 15:56:22 +0200

    fonction preprocess init prete et controlé

--- scripts/preprocess_init.py
+++ scripts/preprocess_init.py
@@ -3,7 +3,7 @@ import pandas as pd
 from preprocess_cpv import extract_cpv_hierarchy_level, add_cpv_hierarchy_column
 
 #retrait des marchés supérieurs à 50 millions et inférieur à 20 milles.
-def drop_outliers(df, min=40000, max=50000000):
+def drop_outliers(df, min=20000, max=50000000):
     df.drop(df[df['montant'] > max].index, inplace=True)
     df.drop(df[df['montant'] < min].index, inplace=True)
     df.drop(df[df['dureeMois'] == 999].index, inplace=True)
@@ -29,51 +29,55 @@ def annee(df):
 
 
 #selection des colonnes
-def columns_selection(df, cat):
+def columns_selection(df, cat, min=20000, max=50000000, top_n=40, level=2):
     if cat == 'pred_montant':
         #selection des colonnes
         df = df[['procedure', 'dureeMois','nature', 'formePrix', 'offresRecues', 'ccag',
             'sousTraitanceDeclaree', 'typeGroupementOperateurs', 'tauxAvance',
-            'origineFrance', 'idAccordCadre', 'dateNotification', 'marcheInnovant', 'codeCPV']]
+            'origineFrance', 'idAccordCadre', 'dateNotification', 'marcheInnovant', 'codeCPV', 'txAvance_cat']]
         #ajout de la colonne codeCPV_2
         df = cpv_2et3(df)
         #drop cpv moins representés
         cpv_group_counts = df['codeCPV_2'].value_counts()
-        top_n = 40
+        top_n = top_n
         top_groups = cpv_group_counts.nlargest(top_n)
         df = df[df['codeCPV_2'].isin(top_groups.index)]
         #drop colonne codeCPV
         df.drop(columns=['codeCPV'], inplace=True)
         #ajout de la colonne année
         df = annee(df)
-        
-        df = drop_outliers(df)
+        #suppression des outiliers (montant sup, inf et dureeMois sup)
+        df = drop_outliers(df, min=min, max=max)
         return df
 
     elif cat == 'marche_sim':
         #selection des colonnes
         df = df[['procedure', 'dureeMois','nature', 'formePrix', 'offresRecues', 'ccag',
             'sousTraitanceDeclaree', 'typeGroupementOperateurs', 'tauxAvance',
-            'origineFrance', 'idAccordCadre', 'montant', 'marcheInnovant', 'codeCPV']]
+            'origineFrance', 'idAccordCadre', 'montant', 'marcheInnovant', 'codeCPV', 'txAvance_cat']]
         #ajout de la colonne codeCPV_2
-        df = add_cpv_hierarchy_column(df, level=2)
+        df = add_cpv_hierarchy_column(df, level=level)
         #drop cpv moins representés
         cpv_group_counts = df['codeCPV_2'].value_counts()
-        top_n = 40
+        top_n = top_n
         top_groups = cpv_group_counts.nlargest(top_n)
         df = df[df['codeCPV_2'].isin(top_groups.index)]
         #drop colonne codeCPV
         df.drop(columns=['codeCPV'], inplace=True)
+        #suppression des outiliers (montant sup, inf et dureeMois sup)
+        df = drop_outliers(df, min=min, max=max)
         return df
 
     elif cat == 'anomalie':
         #selection des colonnes
         df = df[['Ronan : à remplir avec les colonnes que tu souhaites']]
         #ajout de la colonne codeCPV_3
-        df = add_cpv_hierarchy_column(df, level=3)
+        df = add_cpv_hierarchy_column(df, level=level)
         #drop cpv moins representés ? (à ajouter si oui)
         #ajout de la colonne annee
         df = annee(df)
+        #suppression des outiliers (montant sup, inf et dureeMois sup)
+        df = drop_outliers(df, min=min, max=max)
         return df
 
     else:
